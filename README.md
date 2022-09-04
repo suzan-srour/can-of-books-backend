328c8764552372bbf895c6a81ef1a7e5f462d8da
suzan-srour <sroursuzan0@gmail.com>
2022-09-04 21:13:44 +0300

    DB done

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ Books are life-changing. They have the power to enlighten, educate, entertain, h
 ## Use this template
 
 Clone this repo to your own account with the green "Use this template" button. Then, add any collaborators. Now you are ready to start adding features! Deploy your site to let the world know which books have had the greatest impact on you, and what's recommended. Enjoy!
+
+## Feature1
+
+Name of feature: Storage
+
+Estimate of time needed to complete: 1h
+
+Start time: 4
+
+Finish time: 7
+
+Actual time needed to complete: 3 with repo preprations.
--- server.js
+++ server.js
@@ -1,18 +1,99 @@
 'use strict';
-
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
-const app = express();
-app.use(cors());
+const mongoose = require('mongoose');
+const server = express();
+server.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
+server.get('/test', (request, response) => {
 
   response.send('test request received')
 
-})
+}) 
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+mongoose.connect('mongodb://localhost:27017/books-database', {useNewUrlParser: true, useUnifiedTopology: true}); 
+
+const BookSchema = new mongoose.Schema({  // define schema
+title: String,
+description:String,
+status:String
+});
+
+
+const BookModel = mongoose.model('Book', BookSchema); //compile the schem into a model
+
+//seed data (insert initial data)
+async function seedData(){
+  const firstBook = new BookModel({
+    title:"the forty Rules of Love" ,
+    description:"Two parallel tales, one in contemporary times and the other in the thirteenth century, when Rumi encounters his spiritual guide, the traveling dervish known as “Shams of Tabrizi” and how they together embody the eternal message of love poetry ",
+    status:" a novel"
+  })
+
+  const secondBook= new BookModel({
+    title: "the alchemist ",
+    description:" the story of the young Spanish shepherd Santiago on his journey to achieve his dream, which has been repeated more than once, which revolves around a treasure buried in the pyramids in Egypt. ",
+    status:" a novel"
+  })
+
+  const thirdBook = new BookModel({
+    title: " The Da Vinci Code" ,
+    description:"A fictional police mystery and suspense novel by American author Dan Brown published in 2003. The novel has sold 60.5 million copies and is ranked at the top of the New York Times bestseller list " ,
+    status:" a novel"
+  })
+
+  await firstBook.save();
+  await secondBook.save();
+  await thirdBook.save();
+}
+
+ seedData(); //call seedData function
+
+
+//Routes
+server.get('/',homeHandler);
+server.get('/test',testHandler);
+server.get('/books',getBooksHandler);
+server.get('*',defualtHandler);
+
+
+// http://localhost:3010/
+function homeHandler(req,res) {
+  res.send("Hi from the home route");
+}
+
+// http://localhost:3010/test
+function testHandler(req,res) {
+  res.status(200).send("You are requesting the test route");
+}
+
+// http://localhost:3010/*
+function defualtHandler(req,res) {
+  res.status(404).send("Sorry, Page not found");
+}
+
+
+function getBooksHandler(req,res) {
+  Model.find({},(err,result)=>{
+      if(err)
+      {
+          console.log(err);
+      }
+      else
+      {
+          console.log(result);
+          res.send(result);
+      }
+  })
+}
+
+
+
+
+server.get('*', (req,res)=>{
+  res.send("page not found");
+})
+server.listen(PORT, () => console.log(`listening on ${PORT}`));
